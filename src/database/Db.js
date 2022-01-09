31ab48514cb22d2bec658a551ed02bf2eef9f1f5
Sam Erickson <sam@samerickson.dev>
2022-01-09 15:23:37 -0800

    feat[Auth]: Add database connection

---
+++ src/database/Db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2';
+import util from 'util';
+
+import dbConfig from '../config/db.config.js';
+
+const pool = mysql.createPool(dbConfig);
+
+pool.getConnection((error, connection) => {
+	if(error) {
+		console.error(error);
+		if(connection) connection.release
+	} else {
+		console.log(`Connected to database`);
+	}
+});
+
+pool.query = util.promisify(pool.query);
+
+export default pool;
