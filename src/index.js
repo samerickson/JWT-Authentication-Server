956e5f4212475523880be4469b8f1b0751fa2c8e
Sam Erickson <sam@samerickson.dev>
2022-01-09 17:08:35 -0800

    feat[server]: Shut down server gracefully

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 
 import app from './app.js';
+import pool from './database/Db.js';
 
 dotenv.config();
 
@@ -10,5 +11,26 @@ app.get('/', (_req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Process started at localhost:${PORT}`));
+const server = app.listen(PORT, console.log(`Process started at localhost:${PORT}`));
 
+const shutdown = () => {
+
+	console.log('\nClosing database connection');
+	pool.end((error) => {
+		if(error) console.error(`Failed to close database connection - ${error}`);
+	});
+	console.log('Closed database connection');
+
+	console.log('\nShutting down server');
+	if(server.listening) {
+		server.close((error) => {
+			console.error(error);
+			process.exit(1);
+		});
+
+		process.exit(0);
+	}
+}
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
