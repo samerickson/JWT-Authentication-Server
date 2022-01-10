829761f9110e2497f717936770b0da9fe7d159a4
Sam Erickson <sam@samerickson.dev>
2022-01-09 18:22:44 -0800

    feat[app]: Link app components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
+
+import AuthRoutes from './routes/Auth.routes.js';
+import { errorHandler, notFound } from './middleware/errorHandler.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('combined'));
+
+app.use('/api/v1', AuthRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
